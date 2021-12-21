8163906a7450c7e7f1a1139eb5b49a2ea8d74ae0
Michael Guadagni <guadagnimichael@gmail.com>
2021-12-20 20:52:50 -0500

    Javascript Algorithms #1

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,8 +1,36 @@
 package com.careerdevs;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println(getFirstValue(new int[]{1, 2, 3, 4}));
+        System.out.println(Main.addition(3));
+        System.out.println(Main.search(new int[]{1, 2, 3, 4}, 3));
+        System.out.println(convert(6));
+        System.out.println(giveMeSomething("Michael"));
+    }
+    //Algorithm 1
+    public static int getFirstValue(int[] arr) {
+        return arr[0];
+    }
+    //ALgorithm 2
+    public static int addition(int num) {
+        return ++num;
+    }
+    //Algorithm 3
+    public static int search(int arr[], int item) {
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i]==item)
+                return i;
+        }
+        return -1;
+    }
+    //Algorithm 4
+    public static int convert(int minutes) {
+        return minutes * 60;
+    }
+    //Algorithm 5
+    public static String giveMeSomething(String a) {
+        return "something " + a;
     }
 }
